a4327a411d4298e7c8d59f7c9c96282ae8072c97
hrrRafael2 <rafaelhruiz@gmail.com>
2022-08-17 20:16:16 -0500

    add:vite-react

---
+++ CalculadoraApp/src/data/Platillos.js
@@ -0,0 +1,21 @@
+const miMenu = [
+    {
+        id:1,
+        name:'Hamburguesas',
+        precio:100
+    },
+    {
+        id:2,
+        name:'Hamburguesas 2',
+        precio:200
+    },
+    {
+        id:3,
+        name:'Hamburguesas 3',
+        precio:300
+    },
+    
+];
+
+
+export default miMenu;
